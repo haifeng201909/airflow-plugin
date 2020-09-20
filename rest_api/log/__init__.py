627cb97c466f3f7c40d92c1127411c0754b20b57
1726711198@qq.com <1726711198@qq.com>
2020-09-21 00:59:19 +0800

    使用 extra 自定义 logger

--- rest_api/log/__init__.py
+++ rest_api/log/__init__.py
@@ -14,6 +14,20 @@ class MyLogger(Logger):
         Return a logger with the specified name, creating it if necessary.
         If no name is specified, return the root logger.
         """
-        return logging.getLogger(name)
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+        # 创建控制台 handler
+        stream_handler = logging.StreamHandler()
+        # 设置 stream handler 日志级别
+        stream_handler.setLevel(logging.INFO)
+        # 设置日志输出格式
+        formatter_string = "[%(asctime)s] {%(className)s:%(lineno)s " \
+                           "%(funcName)s:%(name)s} " \
+                           "%(levelname)s - %(message)s"
+        formatter = logging.Formatter(formatter_string)
+        stream_handler.setFormatter(formatter)
+        # 将 handler 添加到 logger 对象中
+        logger.addHandler(stream_handler)
+        return logger
 
     pass
--- rest_api/service/mylog_service.py
+++ rest_api/service/mylog_service.py
@@ -1,3 +1,19 @@
 """
 测试自定义 logger
 """
+import logging
+
+from rest_api.log import MyLogger
+
+
+class MyLogTest:
+    """
+    测试自定义日志类的使用
+    """
+
+    @staticmethod
+    def record_log():
+        logger = MyLogger.get_logger("my_log")
+        extra_msg = {"className": "class name"}
+        logger.info("MyLogTest", extra=extra_msg)
+        return "MyLogTest"
