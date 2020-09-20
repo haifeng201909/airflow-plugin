0bda6f5213f5f157454508e6d723df383e1b6693
1726711198@qq.com <1726711198@qq.com>
2020-09-21 00:03:29 +0800

    解决 'Blueprint' object is not callable

--- rest_api/route/mylog_route.py
+++ rest_api/route/mylog_route.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 
+from rest_api.service.mylog_service import MyLogTest
+
 my_log_pb = Blueprint("my_log", __name__)
 
 
-@my_log_pb("/", methods=['GET'])
+@my_log_pb.route("/", methods=['GET'])
 def my_log():
-    pass
+    return MyLogTest.record_log()
